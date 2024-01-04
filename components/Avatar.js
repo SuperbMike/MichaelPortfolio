f3f561962e88fc52f19868cd12fdc8825a059b6f
SuperbMike <michael_byron@hotmail.co.uk>
2024-01-04 17:15:54 +0100

    Portfolio website
    Portfolio website

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,12 @@
+import Image from 'next/image';
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className='hidden xl:flex xl:max-w-none ml-40'> {/* Adjust ml value as needed */}
+      {/* Use correct path and file extension */}
+      <Image src={'/avatar.png'} width={650} height={260} alt='' className='translate-z-0 w-full h-full' />
+    </div>
+  );
 };
 
 export default Avatar;
--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -23,8 +23,117 @@ const testimonialData = [
   },
 ];
 
-const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+// data
+const workSlides = {
+  slides: [
+    {
+      images: [
+        {
+          title: 'title',
+          path: '/thumb1.jpg',
+        },
+        {
+          title: 'title',
+          path: '/thumb2.jpg',
+        },
+        {
+          title: 'title',
+          path: '/thumb3.jpg',
+        },
+        {
+          title: 'title',
+          path: '/thumb4.jpg',
+        },
+      ],
+    },
+    {
+      images: [
+        {
+          title: 'title',
+          path: '/thumb4.jpg',
+        },
+        {
+          title: 'title',
+          path: '/thumb1.jpg',
+        },
+        {
+          title: 'title',
+          path: '/thumb2.jpg',
+        },
+        {
+          title: 'title',
+          path: '/thumb3.jpg',
+        },
+      ],
+    },
+  ],
 };
+
+//import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// next image
+import Image from 'next/image';
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// icons
+import { FaQuoteLeft } from 'react-icons/fa';
+
+//import required modules
+import { Navigation , Pagination } from 'swiper';
+
+// ... (previous imports)
+
+// ... (previous imports)
+// ... (previous imports)
+
+const TestimonialSlider = () => {
+  return (
+    <Swiper
+      navigation={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[ Navigation, Pagination ]}
+      className='h-[400px]'
+    >
+      {testimonialData.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
+              {/* Avatar, name, position */}
+              <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+                <div className='flex flex-col justify-center text-center'>
+                  {/* Avatar */}
+                  <div className='mb-2 mx-auto'>
+                    <Image src={person.image} width={100} height={100} alt=''/>
+                  </div>
+                </div>
+                {/* Name */}
+                <div className='text-lg'>{person.name}</div>
+                {/* Position */}
+                <div className = 'text-[12px] uppercase font-extralight tracking-widest'>{person.position}</div>
+              </div>
+              {/* Quote & message */}
+              <div className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+                {/* Quote icon */} 
+                <div>
+                  <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0 mt-[-40px]' />
+                </div>
+                {/*message*/}
+                <div className='xl:text-lg text-center md:text-left'>{person.message}</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+}
 
 export default TestimonialSlider;
